815f4b3c7ecb07438e58bb5bb2582659b3f06dca
jagantripuragiri <tripuragirijagan27@gmail.com>
2026-08-18 11:34:45 +0530

    refactor: replace hardcoded workspace path with environment variable and update related service imports

--- src/vs/workbench/contrib/void/common/metricsService.ts
+++ src/vs/workbench/contrib/void/common/metricsService.ts
@@ -54,7 +54,7 @@ registerAction2(class extends Action2 {
 		super({
 			id: 'voidDebugInfo',
 			f1: true,
-			title: localize2('voidMetricsDebug', 'Void: Log Debug Info'),
+			title: localize2('voidMetricsDebug', 'Jcode: Log Debug Info'),
 		});
 	}
 	async run(accessor: ServicesAccessor): Promise<void> {
